4f291e6fc1495d61a12a64394c4445c4b749f7e7
ZuluSpirit <serz@ukr.net>
2019-10-14 12:25:03 +0300

    Upd homework for lesson 6. Add Fibonacci sequence and Pascal's triangle sequence from Lesson 6 topics.

--- Homeworks/HW6.py
+++ Homeworks/HW6.py
@@ -54,3 +54,65 @@ for vIndex in range(1, len(vAnyList) + 1):
     vResultList.append(vAnyList[-vIndex])
 
 print('Reverse version 2:\n', vResultList, '\n', sep='')
+
+print('Lesson topics: Fibonacci sequence.\n')
+print('''If n > 1 then (n-1)+(n-2) 
+If n == 1 then 1 
+If n == 0 then 0. 
+''')
+print('Result Fibonacci sequence:\n')
+
+vFibonacciCount = int(input('Input the number of Fibonacci sequence elements: '))
+
+print('')
+
+vResult   = 0
+vPrevious = 0
+vCurrent  = 0
+
+vResultList = []
+
+for i in range(vFibonacciCount):
+    vResult = vPrevious + vCurrent
+
+    if i < 2:
+        vCurrent  = i
+        vResult   = i
+    else:
+        vPrevious = vCurrent
+        vCurrent  = vResult
+
+    vResultList.append(vResult)
+
+print('First {} elements of Fibonacci sequence:\n'.format(vFibonacciCount), vResultList, sep='')
+
+print('Lesson topics: Pascal\'s triangle sequence.\n')
+print('''Pascal’s triangle sequence, given positive int k, returns a list of k
+lists, each representing a floor in the pyramid/triangle. See the following for
+rules: https://en.wikipedia.org/wiki/Pascal%27s_triangle 
+''')
+print('Result Pascal\'s triangle sequence:\n')
+
+vPascalsDepth = int(input('Input the depth of Pascal\'s triangle sequence: '))
+vPascalsPrev  = [] 
+
+print('')
+
+for i in range(vPascalsDepth):
+    vPascalsLine = [] 
+        
+    j = 0
+    
+    while j <= i:        
+        if j == 0 or j == i:
+            vPascalsLine.append(1)
+        else:
+            vPascalsLine.append(vPascalsPrev[j - 1] + vPascalsPrev[j])
+
+        j += 1
+    
+    vPascalsPrev = vPascalsLine.copy()
+
+    print('{}:'.format(i), vPascalsLine)
+
+print('\nThat\'s all Folks!')
